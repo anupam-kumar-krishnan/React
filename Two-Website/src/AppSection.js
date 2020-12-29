1437f55549352e38012742b96989a011e77c4044
Anupam Kumar Krishnan <69143883+anupam-kumar-krishnan@users.noreply.github.com>
2020-12-29 21:48:09 +0530

    AppSection.js🚀🚀

--- Two-Website/src/AppSection.js
+++ Two-Website/src/AppSection.js
@@ -1,21 +1,19 @@
-import React from 'react'
+import React from "react";
 
 const AppSection = () => (
-    <div className="bg-success text-center">
-      <p className="display-4 text-white p-4">Discover the amazing new app</p>
-      <p className="text-white">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, rem?
-      </p>
-      <div className="row  p-4">
-        <div className="col-6 text-right">
-          <button className="btn btn-warning btn-lg">Play Store</button>
-        </div>
-        <button className="btn btn-warning btn-lg">App Store</button>
-        <div className="col-6 text-center"></div>
+  <div className="bg-success text-center">
+    <p className="display-4 text-white p-4">Discover the amazing new app</p>
+    <p className="text-white">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, rem?
+    </p>
+    <div className="row  p-4">
+      <div className="col-6 text-right">
+        <button className="btn btn-warning btn-lg">Play Store</button>
       </div>
+      <button className="btn btn-warning btn-lg">App Store</button>
+      <div className="col-6 text-center"></div>
     </div>
-)
-   
-
+  </div>
+);
 
 export default AppSection;
