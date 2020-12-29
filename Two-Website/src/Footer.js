78e194bc714089ae69a81a30d82fbaa1ff174bdc
Anupam Kumar Krishnan <69143883+anupam-kumar-krishnan@users.noreply.github.com>
2020-12-29 21:42:02 +0530

    Create Footer.js

---
+++ Two-Website/src/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => (
+  <section className="footer  bg-dark text-white">
+    <p className="lead container">Contact us at Instagram</p>
+    <p className="lead container">You can react via email too</p>
+    <p className="lead container">NO COPYRIGHT</p>
+  </section>
+);
+
+export default Footer;
