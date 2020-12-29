d76b32d8456e83607e045612d1c8fd7207251b81
Anupam Kumar Krishnan <69143883+anupam-kumar-krishnan@users.noreply.github.com>
2020-12-29 21:42:34 +0530

    Create HeroSection.js

---
+++ Two-Website/src/HeroSection.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+const HeroSection = () => {
+  return (
+    <div className="jumbotron ">
+      <h1 className="display-4">App looks great!</h1>
+      <p className="lead">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ratione
+        necessitatibus, quas voluptas beatae optio maxime cupiditate at fuga
+        officiis odit rem provident nobis esse magni! Lorem ipsum dolor sit amet
+        consectetur, adipisicing elit. Assumenda velit dolorem repudiandae qui
+        rerum possimus sit animi. Illo, laudantium ea!
+      </p>
+      <hr className="my-4" />
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem,
+        incidunt?
+      </p>
+      <a className="btn btn-success btn-lg" href="#" role="button">
+        React is awesome
+      </a>
+    </div>
+  );
+};
+
+export default HeroSection;
