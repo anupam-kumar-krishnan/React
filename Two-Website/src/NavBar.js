acb3ed3ee4b9ae2a3416655cc7300751f4ce7657
Anupam Kumar Krishnan <69143883+anupam-kumar-krishnan@users.noreply.github.com>
2020-12-29 21:53:10 +0530

    NavBar.js🚀🚀

--- Two-Website/src/NavBar.js
+++ Two-Website/src/NavBar.js
@@ -1,25 +1,31 @@
 import React from "react";
 
 const NavBar = () => {
-    return(
-        <nav className="navbar navbar-expand-lg text-white bg-dark">
-      <a className="navbar-brand text-success" href="#">Navbar</a>
+  return (
+    <nav className="navbar navbar-expand-lg text-white bg-dark">
+      <a className="navbar-brand text-success" href="#">
+        Navbar
+      </a>
 
       <div className="collapse navbar-collapse">
         <ul className="navbar-nav mr-auto">
           <li className="nav-item active">
-            <a className="nav-link text-white" href="#">Home </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link text-white" href="#">About Us</a>
-          </li>
-          <li className="nav-item dropdown">
-            <a className="nav-link text-white">
-              Products
+            <a className="nav-link text-white" href="#">
+              Home{" "}
             </a>
           </li>
           <li className="nav-item">
-            <a className="nav-link disabled" href="#">Meeting</a>
+            <a className="nav-link text-white" href="#">
+              About Us
+            </a>
+          </li>
+          <li className="nav-item dropdown">
+            <a className="nav-link text-white">Products</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link disabled" href="#">
+              Meeting
+            </a>
           </li>
         </ul>
         <form className="form-inline my-2 my-lg-0">
@@ -29,12 +35,15 @@ const NavBar = () => {
             placeholder="Search"
             aria-label="Search"
           />
-          <button className="btn btn-outline-success my-2 my-sm-0" type="submit">
+          <button
+            className="btn btn-outline-success my-2 my-sm-0"
+            type="submit"
+          >
             Search
           </button>
         </form>
       </div>
     </nav>
-    )   
-}
+  );
+};
 export default NavBar;
