b17fe29af9b88d7157765c13b91dd72eb7527122
Anupam Kumar Krishnan <69143883+anupam-kumar-krishnan@users.noreply.github.com>
2021-01-04 16:34:54 +0530

    Updated BuyPage.js🔨🔨

--- Nine- Shopping cart/src/Components/BuyPage.js
+++ Nine- Shopping cart/src/Components/BuyPage.js
@@ -8,7 +8,7 @@ import { Container, Col, Row } from "reactstrap";
 const apiKey = "INSET_YOUR_KEY_HERE";
 
 const url = "https://api.pexels.com/v1/search?query=laptop&per_page=6&page=1";
-const localurl = "https://jsonkeeper.com/b/H80B";
+const localurl = "https://jsonware.com/json/e1289836c6426d74e51d9a94eb231007.json";
 const BuyPage = ({ addInCart }) => {
   const [product, setProduct] = useState([]);
 
@@ -23,6 +23,7 @@ const BuyPage = ({ addInCart }) => {
     const { data } = await Axios.get(localurl, {});
 
     const { photos } = data;
+    console.log(data);
 
     const allProduct = photos.map(photo => ({
       smallImage: photo.src.medium,
