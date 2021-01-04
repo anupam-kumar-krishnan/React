c27fd06333135e779c207a0516ea6e952ef09cf7
Anupam Kumar Krishnan <69143883+anupam-kumar-krishnan@users.noreply.github.com>
2021-01-04 16:36:13 +0530

    Update CartItem.js

--- Nine- Shopping cart/src/Components/CartItem.js
+++ Nine- Shopping cart/src/Components/CartItem.js
@@ -12,11 +12,11 @@ import {
 const CartItem = ({ product, addInCart }) => {
   return (
     <Card className="mt-2 mb-1">
-      <CardImg top height="250" width="100%" src={product.smallImage} />
+      <CardImg top height="200" width="100%" src={product.smallImage} />
       <CardBody className="text-center">
         <CardTitle>{product.productName}</CardTitle>
         <CardText className="secondary">
-          price: RS {product.productPrice}
+          Price: Rs {product.productPrice}
         </CardText>
         <Button color="success" onClick={() => addInCart(product)}>
           Buy Now
